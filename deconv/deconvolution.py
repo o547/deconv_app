38437987fbdb4229b9702a8c90156ff1dc4e2f48
o547 <otashibaura@gmail.com>
2026-02-07 22:59:24 +0900

    　コード整形

--- deconv/deconvolution.py
+++ deconv/deconvolution.py
@@ -4,18 +4,15 @@ import cv2
 
 def create_filtered(fname_in, fname_out, fname_kernel, select=1, input_t=1, epsilon=1):
 
-
     img = cv2.imread(fname_in)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     img = np.float64(img)
     G_fft2 = np.fft.fft2(img)
 
-
-    img_out = np.zeros_like( img )
-    img_out_fft2 = np.zeros_like( img )
+    img_out = np.zeros_like(img)
+    img_out_fft2 = np.zeros_like(img)
     H = img.shape[0]
     W = img.shape[1]
-
 
     img = cv2.imread(fname_kernel)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -27,46 +24,44 @@ def create_filtered(fname_in, fname_out, fname_kernel, select=1, input_t=1, epsi
     H_fft2 = np.fft.fft2(img)
     img_out_fft2 = np.array(img_out_fft2, dtype=complex)
 
-
-
-    if select==0:
-        #単純なdeconvolution
-        #gauss:3 line:2
-        t=complex(10**input_t,0)
-        for v in range(H) :
-            for u in range(W) :
-                if abs(H_fft2[v,u])<t:
-                    img_out_fft2[v,u]=G_fft2[v,u]/t
+    if select == 0:
+        # 単純なdeconvolution
+        # gauss:3 line:2
+        t = complex(10**input_t, 0)
+        for v in range(H):
+            for u in range(W):
+                if abs(H_fft2[v, u]) < t:
+                    img_out_fft2[v, u] = G_fft2[v, u] / t
                 else:
-                    img_out_fft2[v,u]=G_fft2[v,u]/H_fft2[v,u]
+                    img_out_fft2[v, u] = G_fft2[v, u] / H_fft2[v, u]
 
     else:
-        #wiener
-        #gauss:5.5 line:4
-        e=complex(10**epsilon, 0)
-        for v in range(H) :
-            for u in range(W) :
-                kyoyaku=complex(H_fft2[v,u].real,-H_fft2[v,u].imag)
-                img_out_fft2[v,u]=kyoyaku/(H_fft2[v,u]*H_fft2[v,u]+e)*G_fft2[v,u]
-
+        # wiener
+        # gauss:5.5 line:4
+        e = complex(10**epsilon, 0)
+        for v in range(H):
+            for u in range(W):
+                kyoyaku = complex(H_fft2[v, u].real, -H_fft2[v, u].imag)
+                img_out_fft2[v, u] = (
+                    kyoyaku / (H_fft2[v, u] * H_fft2[v, u] + e) * G_fft2[v, u]
+                )
 
     img_out = np.fft.ifft2(img_out_fft2)
-    #値の範囲を[0,255]にする
+    # 値の範囲を[0,255]にする
     img_out = img_out.real
-    max=img_out[0][1]
-    min=img_out[0][1]
-    for v in range( H ) :
-        for u in range( W ) :
-            if u==0 and v==0:
+    max = img_out[0][1]
+    min = img_out[0][1]
+    for v in range(H):
+        for u in range(W):
+            if u == 0 and v == 0:
                 continue
-            if img_out[v,u]>max:
-                max=img_out[v,u]
-            if img_out[v,u]<min:
-                min=img_out[v,u]
+            if img_out[v, u] > max:
+                max = img_out[v, u]
+            if img_out[v, u] < min:
+                min = img_out[v, u]
 
-    for v in range( H ) :
-        for u in range( W ) :
-            img_out[v,u]=(img_out[v,u]-min)/(max-min)*255
-
+    for v in range(H):
+        for u in range(W):
+            img_out[v, u] = (img_out[v, u] - min) / (max - min) * 255
 
     cv2.imwrite(fname_out, abs(img_out))
--- deconv/views.py
+++ deconv/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponseRedirect
 from .psf import create_gaussian
@@ -8,47 +8,82 @@ import cv2
 
 
 # Create your views here.
-filename="garden.png"
-image_height=400
-image_width=600
+filename = "garden.png"
+image_height = 400
+image_width = 600
+
 
 class IndexView(View):
-    def get(self,request):
+    def get(self, request):
         global filename
-        filename="garden.png"
-        return render(request,"deconv/index.html",{"before_image":filename, "after_image":"new"+filename, "sigma":"", "epsilon":"", "t":"", "select":1})
-    def post(self,request):
+        filename = "garden.png"
+        return render(
+            request,
+            "deconv/index.html",
+            {
+                "before_image": filename,
+                "after_image": "new" + filename,
+                "sigma": "",
+                "epsilon": "",
+                "t": "",
+                "select": 1,
+            },
+        )
+
+    def post(self, request):
         global filename
         try:
-            image=request.FILES['image']
-            if(image.name.rfind('.png')!=-1 or image.name.rfind('.jpg')!=-1 or image.name.rfind('.jpeg')!=-1):
+            image = request.FILES["image"]
+            if (
+                image.name.rfind(".png") != -1
+                or image.name.rfind(".jpg") != -1
+                or image.name.rfind(".jpeg") != -1
+            ):
                 fs = FileSystemStorage()
-                extension=image.name.split('.')[1]
-                filename = str(hash(image.name))+"."+extension
+                extension = image.name.split(".")[1]
+                filename = str(hash(image.name)) + "." + extension
                 fs.save(filename, image)
-                im = cv2.imread("static/deconv/image/"+filename)
+                im = cv2.imread("static/deconv/image/" + filename)
                 global image_height
                 global image_width
                 image_height, image_width, _ = im.shape
                 print(str(image_height))
         except:
             print("ファイルは存在しません")
-        sigma=request.POST['sigma']
+        sigma = request.POST["sigma"]
         try:
-            epsilon=request.POST['epsilon']
-            t=1
+            epsilon = request.POST["epsilon"]
+            t = 1
         except:
-            t=request.POST['t']
-            epsilon=1
-        if request.POST['algorithm']=="standard":
-            select=0
+            t = request.POST["t"]
+            epsilon = 1
+        if request.POST["algorithm"] == "standard":
+            select = 0
         else:
-            select=1
-        create_gaussian(image_width,image_height,float(sigma),"static/deconv/image/gaussian.png")
-        create_filtered("static/deconv/image/"+filename,
-                        "static/deconv/image/new"+filename,
-                        "static/deconv/image/gaussian.png",
-                        select=select,input_t=float(t),epsilon=float(epsilon))
-        return render(request,"deconv/index.html",{"before_image":filename, "after_image":"new"+filename, "sigma":sigma, "epsilon":epsilon, "t":t, "select":select})
+            select = 1
+        create_gaussian(
+            image_width, image_height, float(sigma), "static/deconv/image/gaussian.png"
+        )
+        create_filtered(
+            "static/deconv/image/" + filename,
+            "static/deconv/image/new" + filename,
+            "static/deconv/image/gaussian.png",
+            select=select,
+            input_t=float(t),
+            epsilon=float(epsilon),
+        )
+        return render(
+            request,
+            "deconv/index.html",
+            {
+                "before_image": filename,
+                "after_image": "new" + filename,
+                "sigma": sigma,
+                "epsilon": epsilon,
+                "t": t,
+                "select": select,
+            },
+        )
 
-index=IndexView.as_view()
+
+index = IndexView.as_view()
